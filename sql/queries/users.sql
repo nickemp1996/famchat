eac2fc95ec8342b3214d9ea4481f31445f21288c
nickemp1996 <nickemp1996@gmail.com>
2026-01-14 20:52:51 -0700

    added queries and code to add, delete, and get users from/to the database.

---
+++ sql/queries/users.sql
@@ -0,0 +1,20 @@
+-- name: CreateUser :one
+INSERT INTO users (id, created_at, updated_at, name, my_role)
+VALUES (
+    $1,
+    NOW(),
+    NOW(),
+    $2,
+    $3
+)
+RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users
+WHERE name = $1 LIMIT 1;
+
+-- name: DeleteUsers :exec
+DELETE FROM users;
+
+-- name: GetUsers :many
+SELECT * FROM users;
